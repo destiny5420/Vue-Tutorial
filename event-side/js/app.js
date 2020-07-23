867b66f24d4351f522601cc231980b49c38b266e
paper.mac <paperhsiao@gmail.com>
2020-07-23 14:21:59 +0800

    [Add] 新增資料動態增加
    ** important **
    迴圈 v- for
    去頭尾空白 v-model.trim

--- event-side/js/app.js
+++ event-side/js/app.js
@@ -1,26 +1,61 @@
 var data = {
-  name: 'Paper',
-  sex: '男',
-  age: '11-20歲',
-  favor: ['coding', 'game', 'travel'],
-  member: true,
+  input: {
+    name: '',
+    sex: '男',
+    phone: '',
+  },
+  menu: [
+    {
+      name: '蕭分維',
+      sex: '男',
+      phone: '0983580999',
+      link: 'https://www.google.com/',
+    },
+    {
+      name: '簡崇恨',
+      sex: '男',
+      phone: '0988765498',
+      link: 'https://www.google.com/',
+    },
+    {
+      name: '蔡減裕',
+      sex: '男',
+      phone: '0986554872',
+      link: 'https://www.google.com/',
+    },
+    {
+      name: '梁寧偉',
+      sex: '男',
+      phone: '0928862133',
+      link: 'https://www.google.com/',
+    },
+    {
+      name: '林宗佐',
+      sex: '男',
+      phone: '0932765998',
+      link: 'https://www.google.com/',
+    },
+  ],
 };
 
 let vm = new Vue({
-  el: '.main',
-  data: data,
-  computed: {
-    today: function () {
-      return this.menu[this.index];
-    },
-
-    total: function () {
-      return this.menu.length;
-    },
-  },
+  el: '.app',
   methods: {
-    changeIndex: function (index) {
-      this.index = (this.index + index + this.total) % this.total;
+    confirm: function () {
+      if (!this.input.phone) return;
+
+      this.input.name = this.input.name || 'null';
+
+      this.menu.push({
+        name: this.input.name,
+        sex: this.input.sex,
+        phone: this.input.phone,
+        link: '',
+      });
+
+      this.input.name = '';
+      this.input.phone = '';
     },
   },
+  data: data,
 });

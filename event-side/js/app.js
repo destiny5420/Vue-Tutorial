35e4e710c9f8485e3ba3bd3bcf8893e643b614a1
paper.mac <paperhsiao@gmail.com>
2020-07-23 16:38:25 +0800

    [Add] 新增Filter功能菜單
    ** important **
    男女使用v-bind:class 來做顏色區分
    使用computed 來做資料 filter，並使用indexOf是否等於 -1 來判斷是否找不到字

--- event-side/js/app.js
+++ event-side/js/app.js
@@ -4,36 +4,100 @@ var data = {
     sex: '男',
     phone: '',
   },
+  filter: {
+    sex: 'All',
+    content: '',
+  },
   menu: [
     {
       name: '蕭分維',
       sex: '男',
       phone: '0983580999',
       link: 'https://www.google.com/',
+      classAtt: {
+        boy: true,
+        girl: false,
+      },
+    },
+    {
+      name: '蔡芬紅',
+      sex: '女',
+      phone: '0988787654',
+      link: 'https://www.google.com/',
+      classAtt: {
+        boy: false,
+        girl: true,
+      },
     },
     {
       name: '簡崇恨',
       sex: '男',
       phone: '0988765498',
       link: 'https://www.google.com/',
+      classAtt: {
+        boy: true,
+        girl: false,
+      },
     },
     {
       name: '蔡減裕',
       sex: '男',
       phone: '0986554872',
       link: 'https://www.google.com/',
+      classAtt: {
+        boy: true,
+        girl: false,
+      },
     },
     {
       name: '梁寧偉',
       sex: '男',
       phone: '0928862133',
       link: 'https://www.google.com/',
+      classAtt: {
+        boy: true,
+        girl: false,
+      },
+    },
+    {
+      name: '梁世襲',
+      sex: '女',
+      phone: '0928862133',
+      link: 'https://www.google.com/',
+      classAtt: {
+        boy: false,
+        girl: true,
+      },
+    },
+    {
+      name: '蔡玉如',
+      sex: '女',
+      phone: '0989754734',
+      link: 'https://www.google.com/',
+      classAtt: {
+        boy: false,
+        girl: true,
+      },
+    },
+    {
+      name: '蔡熟分',
+      sex: '女',
+      phone: '0988645338',
+      link: 'https://www.google.com/',
+      classAtt: {
+        boy: false,
+        girl: true,
+      },
     },
     {
       name: '林宗佐',
       sex: '男',
       phone: '0932765998',
       link: 'https://www.google.com/',
+      classAtt: {
+        boy: true,
+        girl: false,
+      },
     },
   ],
 };
@@ -45,16 +109,36 @@ let vm = new Vue({
       if (!this.input.phone) return;
 
       this.input.name = this.input.name || 'null';
-
       this.menu.push({
         name: this.input.name,
         sex: this.input.sex,
         phone: this.input.phone,
         link: '',
+        classAtt: {
+          boy: this.input.sex === '男' ? true : false,
+          girl: this.input.sex === '男' ? false : true,
+        },
       });
-
-      this.input.name = '';
-      this.input.phone = '';
+    },
+  },
+  computed: {
+    sexFilter: function () {
+      if (this.filter.sex !== 'All') {
+        return this.menu.filter((item) => {
+          return item.sex === this.filter.sex;
+        });
+      } else return this.menu;
+    },
+    inputFilter: function () {
+      if (this.filter.content) {
+        return this.sexFilter.filter((item) => {
+          let srcContent = item.name.toLowerCase();
+          let filterContent = this.filter.content;
+          return srcContent.indexOf(filterContent) !== -1;
+        });
+      } else {
+        return this.sexFilter;
+      }
     },
   },
   data: data,

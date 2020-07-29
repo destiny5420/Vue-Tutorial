f6c8654fca8d8c1fd6531f1955a3ed46bc2546d6
paper.window <destiny5420@gmail.com>
2020-07-29 22:02:00 +0800

    [Add] 初版分類 [Modify] 修正客製化的版面

--- todo-list/src/main.js
+++ todo-list/src/main.js
@@ -1,12 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { sync } from 'vuex-router-sync';
+import Vue from 'vue';
+import App from './main.vue';
+import router from './router';
+import store from './store';
+sync(store, router);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
--- todo-list/src/store/index.js
+++ todo-list/src/store/index.js
@@ -1,11 +1,85 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const LS = {
+  load: function() {
+    return JSON.parse(localStorage.getItem('vue-todo') || '[]');
+  },
+  save: function(data) {
+    localStorage.setItem('vue-todo', JSON.stringify(data));
+  },
+};
+
+const filter = {
+  all: function(data) {
+    return data;
+  },
+  uncomplete: function(data) {
+    return data.filter((data) => {
+      return data.complete === false;
+    });
+  },
+  complete: function(data) {
+    return data.filter((data) => {
+      return data.complete === true;
+    });
+  },
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  strict: true,
+  state: {
+    todos: [
+      { content: 'todo-content', complete: false },
+      { content: 'todo-content', complete: true },
+      { content: 'todo-content', complete: false },
+    ],
+  },
+  getters: {
+    todoIndex: function(state) {
+      // return filter[state.route.name](state.todos).map((item) => {
+      //   state.todos.indexOf(item);
+      // });
+
+      return filter[state.route.name](state.todos).map((item) =>
+        state.todos.indexOf(item)
+      );
+    },
+  },
+  mutations: {
+    SetTodos: function(state, data) {
+      state.todos = data;
+
+      // Save local storage.
+      LS.save(state.todos);
+    },
+    AddTodos: function(state, data) {
+      state.todos.push(data);
+
+      // Save local storage.
+      LS.save(state.todos);
+    },
+    UpdateTodos: function(state, data) {
+      // this.$set(state.todos, index, data);
+      state.todos[data.index] = data.content;
+
+      // Save local storage.
+      LS.save(state.todos);
+    },
+    DeleteTodos: function(state, index) {
+      state.todos.splice(index, 1);
+
+      // Save local storage.
+      LS.save(state.todos);
+    },
+  },
+  actions: {
+    Init: function(context) {
+      // Loading local storage.
+      context.commit('SetTodos', LS.load());
+    },
+  },
+  modules: {},
 });

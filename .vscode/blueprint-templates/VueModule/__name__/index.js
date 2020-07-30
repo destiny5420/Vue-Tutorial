31de06425e0326a4c8a9764d03b870ba80840852
paper.mac <paperhsiao@gmail.com>
2020-07-30 10:36:39 +0800

    [Add] 新增Blueprint-templates feature

---
+++ .vscode/blueprint-templates/VueModule/__name__/index.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'default',
+  data: function () {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  // life cycle
+  beforeCreate: function () {},
+  created: function () {},
+  beforeMounted: function () {},
+  mounted: function () {},
+  beforeUpdate: function () {},
+  updated: function () {},
+  beforeDestroy: function () {},
+  Destroy: function () {},
+};
